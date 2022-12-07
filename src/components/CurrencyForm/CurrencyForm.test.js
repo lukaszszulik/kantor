25a61e19f8f3e4d9f4084b0c0ea921f246e66680
Lukasz Szulik <lukaszgit@o2.pl>
2022-12-07 18:51:17 +0100

    add currencyFormTest fix

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -3,6 +3,11 @@ import CurrencyForm from './CurrencyForm';
 import userEvent from '@testing-library/user-event';
 import { render, screen, cleanup } from '@testing-library/react';
 
+describe('Component CurrencyForm', () => {
+  it('should render without crashing', () => {
+    render(<CurrencyForm action={() => {}} />);
+  });
+
   it('should run action callback with proper data on form submit', () => {
 
     const testCases = [
@@ -45,6 +50,6 @@ import { render, screen, cleanup } from '@testing-library/react';
 
   // unmount component
   cleanup();
-}
-
+    }
+  });
 });
